7480cabe43f55359b1fb6d4bc55a885a96ceafc4
aruboss <minhhieu1096@gmail.com>
2018-08-26 17:33:39 +0700

    phan chia component, add them thu vien

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TaskForm from './components/TaskForm'
+import Control from './components/Control'
+import TaskList from './components/TaskList'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Title Page</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossOrigin="anonymous" />
+        <div className="container">
+          <div className="text-center">
+            <h1>Quản Lý Công Việc</h1>
+            <hr />
+          </div>
+          <div className="row">
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+            {/*Form*/}
+              <TaskForm />
+            </div>
+            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              <button type="button" className="btn btn-primary">
+                <span className="fa fa-plus mr-5" />Thêm Công Việc
+              </button> 
+              {/*tìm kiếm và sắp xếp */}
+                <Control />
+              <div className="row mt-15">
+                <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                  <TaskList />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
